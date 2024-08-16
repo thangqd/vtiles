b435c3feb7b0b779ef5a10241355187dcb8b4bcc
thangqd <quachdongthang@gmail.com>
2024-08-16 21:23:37 +0700

    update mgrs grid

--- tiles_util/grid/georefgrid.py
+++ tiles_util/grid/georefgrid.py
@@ -1,4 +1,6 @@
 
 from tiles_util.utils.geocode import georef
-georef_code = georef.encode(10.534535345,106.4343242,5)
+georef_code = georef.encode(10.534535345,106.4343242,3)
 print(georef_code)
+georef_decode = georef.decode(georef_code)
+print(georef_decode)
--- tiles_util/grid/mgrsgrid.py
+++ tiles_util/grid/mgrsgrid.py
@@ -7,7 +7,10 @@
 # https://storymaps.arcgis.com/stories/842edf2b4381438b9a4edefed124775b
 # https://github.com/dnlbaldwin/React-Leaflet-MGRS-Graticule
 # https://dnlbaldwin.github.io/React-Leaflet-MGRS-Graticule/
-
+# https://earth-info.nga.mil/index.php?dir=coordsys&action=mgrs-100km-polyline-dloads
+# https://mgrs-data.org/metadata/
+# https://ufl.maps.arcgis.com/apps/dashboards/2539764e24e74bd78f265f49c7adc2d1
+# https://earth-info.nga.mil/index.php?dir=coordsys&action=gars-20x20-dloads
 # from tiles_util.utils.geocode import mgrs
 # mgrs_code = mgrs.toMgrs(10.63038542, 106.12923131,3)
 # # mgrs_code = mgrs.toMgrs(-84.65698112, -80.69068228,3)
