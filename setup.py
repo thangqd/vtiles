950aaf149933b1aac6e72d4293823e649cf288dc
thangqd <quachdongthang@gmail.com>
2024-09-03 20:31:58 +0700

    reorganize the project

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requirements = [
 
 setup(
     name='tiles_util',
-    version='1.0.6',
+    version='1.1.0',
     author = 'Thang Quach',
     author_email= 'quachdongthang@gmail.com',
     url='https://github.com/thangqd/tiles_util',
@@ -57,17 +57,7 @@ setup(
             'servevectormbtiles=tiles_util.server.servevectormbtiles:main',       
             'servepmtiles=tiles_util.server.servepmtiles:main',                   
             
-            'vcode2geojson = tiles_util.vcode:vcode2geojson_cli',  
-            'vgrid = tiles_util.vcode.vgrid:main',      
-    
-            'vectortilegrid = tiles_util.utils.grid.vectortilegrid:main',
-            'pluscodegrid = tiles_util.utils.grid.pluscodegrid:main',
-            'geohashgrid = tiles_util.utils.grid.geohashgrid:main',
-            'h3grid = tiles_util.utils.grid.h3grid:main',
-            's2grid = tiles_util.utils.grid.s2grid:main',
-            'maidenheadgrid = tiles_util.utils.grid.maidenheadgrid:main',
-            'mgrsgrid = tiles_util.utils.grid.mgrsgrid:main',
-            
+           
             'pmtilesinfo = tiles_util.utils.pmtilesinfo:main',
             'pmtiles2folder = tiles_util.utils.pmtiles2folder:main',
             'pmtiles2mbtiles = tiles_util.utils.pmtiles2mbtiles:main',           
